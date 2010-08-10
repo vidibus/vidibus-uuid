f08ed00fd0f45a58fa2dbf6a59ad6135e8fe046e
Andre Pankratz <andre@webwarelab.com>
2010-08-10 15:25:58 +0200

    Define Engine only if Rails is available

--- lib/vidibus-uuid.rb
+++ lib/vidibus-uuid.rb
@@ -2,8 +2,10 @@ require "vidibus/uuid"
 require "vidibus/validate_uuid"
 require "vidibus/uuid/mongoid"
 
-module Vidibus::Uuid
-  class Engine < ::Rails::Engine; end  
+if defined?(Rails)
+  module Vidibus::Uuid
+    class Engine < ::Rails::Engine; end  
+  end
 end
 
 ActiveModel::Validations.send(:include, Vidibus::ValidateUuid)
